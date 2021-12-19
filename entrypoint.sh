ec6c479f15443fd6fb78d2c05429025ed0b8b9bf
David Hernando <david@whiletrue.org>
2021-12-19 16:17:57 +0100

    Added publish to gh-pages
    Copied from: https://github.com/maxheld83/ghpages/blob/master/entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,29 @@
 #!/usr/bin/env sh
 
+set -e
+
+echo "#################################################"
+echo "Generating documenation with DOCFX..."
 mono "/docfx/docfx.exe" "$@"
+
+echo "#################################################"
+echo "Changing directory to 'BUILD_DIR' $BUILD_DIR ..."
+cd $BUILD_DIR
+
+echo "#################################################"
+echo "Now deploying to GitHub Pages..."
+REMOTE_REPO="https://${GH_PAT}@github.com/${GITHUB_REPOSITORY}.git" && \
+REMOTE_BRANCH="gh-pages" && \
+git init && \
+git config user.name "${GITHUB_ACTOR}" && \
+git config user.email "${GITHUB_ACTOR}@users.noreply.github.com" && \
+if [ -z "$(git status --porcelain)" ]; then
+    echo "Nothing to commit" && \
+    exit 0
+fi && \
+git add . && \
+git commit -m 'Deploy to GitHub Pages' && \
+git push --force $REMOTE_REPO master:$REMOTE_BRANCH && \
+rm -fr .git && \
+cd $GITHUB_WORKSPACE && \
+echo "Content of $BUILD_DIR has been deployed to GitHub Pages."
